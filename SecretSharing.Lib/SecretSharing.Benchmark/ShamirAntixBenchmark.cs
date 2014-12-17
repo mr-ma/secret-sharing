790f37d960d942453daa3164879fc4bf4dccf721
MAhmadvand <mohsen.ahmadvand@brainloop.com>
2014-12-17 16:24:47 +0100

    missing files

---
+++ SecretSharing.Lib/SecretSharing.Benchmark/ShamirAntixBenchmark.cs
@@ -0,0 +1,52 @@
+﻿using SecretSharing.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antix.Testing;
+using System.Threading;
+namespace SecretSharing.Benchmark
+{
+    public class SecretSharingBenchmarkReport
+    {
+        public int n;
+        public int k;
+        public TimeSpan avg;
+        public int keyLength;
+    }
+    public class ShamirAntixBenchmark
+    {
+        string[] keys = new string[]{"12345678"
+            ,"1234567812345678"
+            ,"12345678123456781234567812345678"
+           // ,"1234567812345678123456781234567812345678123456781234567812345678"
+        };
+        String key64bit = "12345678";
+        String key128bit = "1234567812345678";
+        String key256bit = "12345678123456781234567812345678";
+        String key512bit = "1234567812345678123456781234567812345678123456781234567812345678";
+
+        
+        public IEnumerable< String> BenchmarkMe()
+        {
+            var score = new SecretSharingCoreTests();
+            List<SecretSharingBenchmarkReport> results = new List<SecretSharingBenchmarkReport>();
+            for (int k = 1; k <= 10; k++)
+			{
+                for (int n = 10; n <= 50; n+=5)
+                {
+                    for (int i = 0; i < keys.Length; i++)
+			        {
+			             var re = Antix.Testing.Benchmark.Run(() => score.TestDivideSecret(n,k*5,keys[i]), 10);
+                         results.Add(new SecretSharingBenchmarkReport() { n = n, k = k*5, avg = re.Average, keyLength = keys[i].Length * 8 });
+			        }
+                }
+			}
+            var orderedResults = results.OrderBy(po => po.keyLength).ThenBy(po => po.n).ThenBy(po => po.k); 
+
+
+            return orderedResults.Select(po => String.Format("keylength:{0}  n:{1} k:{2}  avg:{3}", po.keyLength, po.n, po.k, po.avg));
+        }
+    }
+}
---
+++ SecretSharing.Lib/SecretSharing.Benchmark/ShamirBenchmark.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pUnit;
+using SecretSharing.Test;
+
+
+namespace SecretSharing.Benchmark
+{
+    [ProfileClass]
+    public class ShamirBenchmark
+    {
+
+        String key64bit = "12345678";
+        String key128bit = "1234567812345678";
+        String key256bit = "12345678123456781234567812345678";
+        String key512bit = "1234567812345678123456781234567812345678123456781234567812345678";
+
+        [ProfileMethod(100)]
+        public void GenerateShare64bitKey()
+        {
+            int n = 10;
+            int k = 5;
+            var score = new SecretSharingCoreTests();
+            score.TestDivideSecret(n, k, key64bit);
+        }
+         [ProfileMethod(100)]
+        public void GenerateShare128bitKey()
+        {
+            int n = 10;
+            int k = 5;
+            var score = new SecretSharingCoreTests();
+            score.TestDivideSecret(n, k, key128bit);
+        }
+         [ProfileMethod(100)]
+         public void GenerateShare256bitKey()
+         {
+             int n = 10;
+             int k = 5;
+             var score = new SecretSharingCoreTests();
+             score.TestDivideSecret(n, k, key256bit);
+         }
+         [ProfileMethod(100)]
+         public void GenerateShare512bitKey()
+         {
+             int n = 10;
+             int k = 5;
+             var score = new SecretSharingCoreTests();
+             score.TestDivideSecret(n, k, key512bit);
+         }
+    }
+}
